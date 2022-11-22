1507a60057d5180bd0b6a563dd59d61c35b89cef
Peter Krysinski <pakrysin@gmail.com>
2022-11-22 14:36:23 -0700

    Just finished tutoring session.  Found out new ways to dynamically add elements to the page, add event listeners, and add classes for each element rendered.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
 var currentDayEL = $('#currentDay');
-var hour9EL = $('#hour-9');
-var hour10EL = $('#hour-10');
-var hour11EL = $('#hour-11');
-var hour12EL = $('#hour-12');
-var hour1EL = $('#hour-1');
-var hour2EL = $('#hour-2');
-var hour3EL = $('#hour-3');
-var hour4EL = $('#hour-4');
-var hour5EL = $('#hour-5');
-var currentHour;
+var plannerEL = $('#planner');
+// START - Variables for static rows (don't want!)
 
+// END -  Variables for static rows (don't want!)
+// START - Variable for dynamic rows
+var workHours = ["9a","10a","11a","12p","1p","2p","3p","4p","5p"];
+// END - Variable for dynamic rows
 
 // Displays the current date in the currentDay component
 function displayDay() {
@@ -17,23 +13,62 @@ function displayDay() {
   currentDayEL.text(today);
 };
 
-currentHour = dayjs().format('h a');
-
-
-
+var currentHour = dayjs().hour();
 // TODO: Add code to apply the past, present, or future class to each time
 // block by comparing the id to the current hour. HINTS: How can the id
 // attribute of each time-block be used to conditionally add or remove the
 // past, present, and future classes? How can Day.js be used to get the
 // current hour in 24-hour time?
 // -----------------------------------------------------------------------------
-// Dynamically changes the background color of any given row depending on if that hour has passed or not
-function setColors() {
-  var divs = document.getElementsByClassName('description'); //No idea if this is right
-  for (var i = 0; i < divs.length; i += 1) {
-    // Need to somehow get the id="hour-9" for instance, in the first row, to check the value
-    // and compare it to the currentHour to see what color it should be set to.
+// Dynamically build the rows based on number of workHours there are, and change the background color of any given row depending on if that hour has passed or not.
+function saveToStorage(key, value){
+  console.log(key, value)
+  localStorage.setItem(key,value)
+}
+
+function renderStorage(key){
+  return JSON.parse(localStorage.getItem(key)) || ''
+}
+
+function buildRows() {
+  for (var i = 0; i < workHours.length; i += 1) {
+    var hourRow = workHours[i];
+
+    // Create row elements
+    var divRowEL = document.createElement('div');
+    var divHourEL = document.createElement('div');
+    divHourEL.textContent = hourRow;
+    var textAreaEL = document.createElement('textarea');
+    var saveButtonEL = document.createElement('button');
+
+    //divRowEL.addClass('row time-block');
+    divRowEL.classList.add("row", "time-block");
+    divHourEL.classList.add("col-2", "col-md-1", "hour", "text-center", "py-3");
+    textAreaEL.classList.add("col-8", "col-md-10", "description");
+    saveButtonEL.classList.add("btn", "saveBtn", "col-2", "col-md-1");
+    saveButtonEL.click(function(event){
+      event.preventDefault()
+      // console.log(event.target.previousSibling.value)
+      saveToStorage(hourRow,event.target.previousSibling.value)
+    })
+
+    // Append a class depending on time of day vs. row hour displayed
+    if(i+9 < currentHour){
+      divRowEL.classList.add('past');
+    }
+    else if (i+9 == currentHour){
+      divRowEL.classList.add('present');
+    } else {
+      divRowEL.classList.add('future');
+    };
+
+    divRowEL.append(divHourEL,textAreaEL,saveButtonEL);
+    plannerEL.append(divRowEL);
+
   };
+
+  //This is being used to get the current hour, and compare it to the workHours rows to determine if they should be grey, red, or green.
+
 };
 
 
@@ -51,6 +86,8 @@ function displaySchedule() {
 
 };
 
+
+
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
@@ -59,17 +96,14 @@ function displaySchedule() {
 // useful when saving the description in local storage?
 // -----------------------------------------------------------------------------
 // Event listener to save data to localStorage on click of save button (per row)
-$(".saveBtn").on("click", function(event) {
-  event.preventDefault();
+// $(".saveBtn").on("click", function(event) {
+//   event.preventDefault();
 
-  saveSchedule();
-  displaySchedule();
-})
-
-
+//   saveSchedule();
+//   displaySchedule();
+// });
 
 
-// Immediately call the displayDay function to have the current date 
-displayDay();
-setColors();
-displaySchedule();
+displayDay();   // Display the current date in the header
+buildRows();    // Dynamically build the rows based on workHours variable, and set colors
+displaySchedule();  // Fetch the schedule from local storage and display to the UI
